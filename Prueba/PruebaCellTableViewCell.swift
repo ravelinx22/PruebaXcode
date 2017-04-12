d4d9ae91256b5e6cc2d349a5bae3e55a8d666a98
William Ravelo <drummerwilliam@gmail.com>
2017-04-12 10:22:17 -0500

    first commit

---
+++ Prueba/PruebaCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PruebaCellTableViewCell.swift
+//  Prueba
+//
+//  Created by William Ravelo on 1/04/17.
+//  Copyright © 2017 WilliamRavelo. All rights reserved.
+//
+
+import UIKit
+
+class PruebaCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
